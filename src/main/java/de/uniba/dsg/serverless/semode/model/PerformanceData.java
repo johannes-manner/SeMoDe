d7c18dcfb070dcc04b149b601d5563de9859eb79
johannes-manner <johannes.manner@web.de>
2018-04-11 13:45:35 +0200

    Fixed the starting time problem (format was corrupted in the .csv files and the presentation in a calculation program was insufficent) with the LocalDateTime class of Java8

--- src/main/java/de/uniba/dsg/serverless/semode/model/PerformanceData.java
+++ src/main/java/de/uniba/dsg/serverless/semode/model/PerformanceData.java
@@ -1,25 +1,29 @@
 package de.uniba.dsg.serverless.semode.model;
 
-import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class PerformanceData {
 	
 	private final String functionName;
 	private final String logStream;
 	private final String requestId;
-	private final Date startTime;
+	private final LocalDateTime startTime;
 	private final double preciseDuration;
 	private final int billedDuration;
 	private final int memorySize;
 	private final int memoryUsed;
 	
 	private static final String CSV_SEPARATOR = ";";
+	private static final DateTimeFormatter CSV_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss");
 	
 	public PerformanceData() {
 		this.functionName = "";
 		this.logStream = "";
 		this.requestId = "";
-		this.startTime = new Date();
+		this.startTime = LocalDateTime.MIN;
 		this.preciseDuration = -1;
 		this.billedDuration = -1;
 		this.memorySize = -1;
@@ -31,7 +35,7 @@ public class PerformanceData {
 		this.functionName = functionName;
 		this.logStream = logStream;
 		this.requestId = requestId;
-		this.startTime = new Date(startTime);
+		this.startTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(startTime), ZoneId.systemDefault());
 		this.preciseDuration = preciseDuration;
 		this.billedDuration = billedDuration;
 		this.memorySize = memorySize;
@@ -50,12 +54,14 @@ public class PerformanceData {
 				"StartTime" + CSV_SEPARATOR + "PreciseDuration" + CSV_SEPARATOR + "BilledDuration" + CSV_SEPARATOR +
 				"MemorySize" + CSV_SEPARATOR + "MemoryUsed";
 	}
+	
+	
 
 	public String toCSVString() {
 		return this.functionName + CSV_SEPARATOR +
 				this.logStream + CSV_SEPARATOR + 
 				this.requestId + CSV_SEPARATOR +
-				this.startTime + CSV_SEPARATOR +
+				this.startTime.format(CSV_FORMATTER) + CSV_SEPARATOR +
 				this.preciseDuration + CSV_SEPARATOR +
 				this.billedDuration + CSV_SEPARATOR +
 				this.memorySize + CSV_SEPARATOR + 
