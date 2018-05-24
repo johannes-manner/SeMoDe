67bb6191552c6d4237d9c3b3abd7d9838c9c06db
johannes-manner <johannes.manner@web.de>
2018-05-24 14:20:55 +0200

    Integrate platformId into function trigger

--- src/main/java/de/uniba/dsg/serverless/benchmark/FunctionTrigger.java
+++ src/main/java/de/uniba/dsg/serverless/benchmark/FunctionTrigger.java
@@ -1,5 +1,6 @@
 package de.uniba.dsg.serverless.benchmark;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +17,10 @@ import javax.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+
 import de.uniba.dsg.serverless.model.SeMoDeException;
 
 public class FunctionTrigger implements Callable<String> {
@@ -24,6 +29,11 @@ public class FunctionTrigger implements Callable<String> {
 	private static final Logger logger  = LogManager.getLogger(FunctionTrigger.class.getName());
 	
 	private static final String CSV_SEPARATOR = System.getProperty("CSV_SEPARATOR");
+
+	// used in the response header element to map the executions on the cloud platform
+	private static final String PLATFORM_ID = "platformId";
+	
+	private static final ObjectReader jsonReader = new ObjectMapper().reader();
 		
 	private final String host;
 	private final String path;
@@ -47,8 +57,10 @@ public class FunctionTrigger implements Callable<String> {
 
 	@Override
 	public String call() throws SeMoDeException {
+		
 		String uuid = UUID.randomUUID().toString();
 		logger.info("START" + CSV_SEPARATOR + uuid);
+		
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target(host).path(path);
 
@@ -59,9 +71,26 @@ public class FunctionTrigger implements Callable<String> {
 		Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
 				.post(Entity.entity(jsonInput,
 						MediaType.APPLICATION_JSON));
-		String responseValue = response.getStatus() + " " + response.getEntity();
+		
+		// the response entity has to be a json representation with a platformId and a result key
+		String responseEntity = response.readEntity(String.class);
+		String platformId = "";
+		
+		try {
+			JsonNode responseNode = jsonReader.readTree(responseEntity);
+			platformId = responseNode.get(PLATFORM_ID).asText();
+		} catch (IOException e) {
+			// swallow the exception, because the platformId is an optional parameter and not 
+			// only relevant when linking the local and remote execution on the platform
+			logger.warn("Error parsing the response from the server. The response should be a json.");
+		}
+		
+		
+		String responseValue = response.getStatus() + " " + responseEntity;
 
 		logger.info("END" + CSV_SEPARATOR + uuid);
+		logger.info("PLATFORMID" + CSV_SEPARATOR + uuid + CSV_SEPARATOR  + platformId);
+		
 		return responseValue;
 	}
 
