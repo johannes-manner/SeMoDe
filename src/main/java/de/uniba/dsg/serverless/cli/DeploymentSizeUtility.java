aa52f40c7843f9b60e4cc8ea670e8267645d92ef
Martin Endreß <martin.endress@stud.uni-bamberg.de>
2018-06-06 18:22:05 +0200

    fill zip files with random characters
    Signed-off-by: Martin Endreß <martin.endress@stud.uni-bamberg.de>

--- src/main/java/de/uniba/dsg/serverless/cli/DeploymentSizeUtility.java
+++ src/main/java/de/uniba/dsg/serverless/cli/DeploymentSizeUtility.java
@@ -1,9 +1,18 @@
 package de.uniba.dsg.serverless.cli;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -14,8 +23,10 @@ import de.uniba.dsg.serverless.model.SeMoDeException;
 
 public class DeploymentSizeUtility extends CustomUtility {
 
-	private final String FILL_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	private final char[] FILL_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+			.toCharArray();
 	private final String DOUBLE_LINEBREAK = "\n\n";
+	private final long BLOCK_SIZE = 10_000_000l;
 
 	private static final Logger logger = LogManager.getLogger(DeploymentSizeUtility.class.getName());
 
@@ -41,20 +52,33 @@ public class DeploymentSizeUtility extends CustomUtility {
 		}
 		String commentStart = args.get(2);
 
-		File file = new File(fileName);
-		if (!file.exists() || !file.isFile()) {
-			logger.fatal("File must be an existing file.");
+		Path path;
+		long fileSize;
+		try {
+			path = Paths.get(fileName);
+			if (!Files.exists(path) || !Files.isRegularFile(path) || !Files.isReadable(path)) {
+				logger.fatal("File must be an existing readable file.");
+				return;
+			}
+			fileSize = Files.size(path);
+		} catch (InvalidPathException e) {
+			logger.fatal("Path is invalid.", e);
+			return;
+		} catch (IOException e) {
+			logger.fatal("Filesize could not be determined.", e);
 			return;
 		}
-
-		if (file.length() > desiredLength) {
+		if (fileSize > desiredLength) {
 			logger.fatal("Length of the file exceeds the desired length.");
 			return;
 		}
 
-		long remainingLength = desiredLength - file.length();
+		long remainingLength = desiredLength - fileSize;
 		try {
-			fillFile(file, remainingLength, commentStart);
+			String content = DOUBLE_LINEBREAK + commentStart;
+			addContent(path, content);
+			remainingLength -= content.length();
+			addRandomContentToFile(path, remainingLength);
 		} catch (SeMoDeException e) {
 			logger.fatal(e.getMessage());
 			return;
@@ -62,30 +86,61 @@ public class DeploymentSizeUtility extends CustomUtility {
 		logger.info("Size of file " + fileName + " successfully increased to " + desiredLength + ".");
 	}
 
-	private void fillFile(File file, long remainingLength, String commentStart) throws SeMoDeException {
-		try (FileWriter fw = new FileWriter(file, true); BufferedWriter writer = new BufferedWriter(fw)) {
-			if (remainingLength < (DOUBLE_LINEBREAK.length() + commentStart.length())) {
-				while (remainingLength > 0) {
-					writer.write(' ');
-					remainingLength--;
-				}
-			}
-			else {
-				writer.write(DOUBLE_LINEBREAK);
-				remainingLength -= DOUBLE_LINEBREAK.length();
-				writer.write(commentStart);
-				remainingLength -= commentStart.length();
-
-				Random random = new Random();
-				while (remainingLength > 0) {
-					int index = random.nextInt(FILL_CHARACTERS.length());
-					writer.write(FILL_CHARACTERS.charAt(index));
-					remainingLength--;
-				}
-			}
+	private void addContent(Path path, String content) throws SeMoDeException {
+		try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND,
+				StandardOpenOption.CREATE)) {
+			writer.write(content);
+			writer.flush();
+			writer.close();
 		} catch (IOException e) {
 			throw new SeMoDeException("Writing to file failed.", e);
 		}
+	}
+
+	private void addRandomContentToFile(Path path, long length) throws SeMoDeException {
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder();
+
+		while (length > 0) {
+			int index = random.nextInt(FILL_CHARACTERS.length);
+			sb.append(FILL_CHARACTERS[index]);
+			length--;
+		}
+		addContent(path, sb.toString());
+	}
+
+	/**
+	 * 
+	 * @see <a href=
+	 *      "https://docs.oracle.com/javase/8/docs/technotes/guides/io/fsp/zipfilesystemprovider.html">Zip
+	 *      File System Provider</a>
+	 * @param path
+	 * @throws SeMoDeException
+	 */
+	private void fillZip(String path, long length) throws SeMoDeException {
+		Path zipPath = Paths.get(path);
+		URI uri = URI.create("jar:" + zipPath.toUri());
+		try (FileSystem system = FileSystems.newFileSystem(uri, new HashMap<>())) {
+			Path pathInZipfile = system.getPath("/SomeTextFile.txt");
+			for (int i = 0; i < length / BLOCK_SIZE; i++) {
+				addRandomContentToFile(pathInZipfile, BLOCK_SIZE);
+			}
+		} catch (IOException | FileSystemNotFoundException e) {
+			throw new SeMoDeException(e.getMessage(), e);
+		}
+		
+		try {
+			while (length > Files.size(zipPath)) {
+				try (FileSystem system = FileSystems.newFileSystem(uri, new HashMap<>())) {
+					Path pathInZipfile = system.getPath("/SomeTextFile.txt");
+					long nextPackage = length - Files.size(zipPath);
+					addRandomContentToFile(pathInZipfile, nextPackage);
+				}
+			}
+		} catch (IOException|FileSystemNotFoundException e) {
+			throw new SeMoDeException(e.getMessage(), e);
+		}
+
 	}
 
 }
