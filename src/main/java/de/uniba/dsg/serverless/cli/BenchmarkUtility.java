e216c19983bf932a5b6ac1fafb75b3b04bcbcc90
Martin Endress <martin.endress@stud.uni-bamberg.de>
2018-06-10 11:17:39 +0200

    Implementation of two new modes for the Benchmarking Utility
    Modes "sequentialChangingInterval" and "sequentialChangingWait".
    
    Signed-off-by: Martin Endress <martin.endress@stud.uni-bamberg.de>

--- src/main/java/de/uniba/dsg/serverless/cli/BenchmarkUtility.java
+++ src/main/java/de/uniba/dsg/serverless/cli/BenchmarkUtility.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
@@ -28,6 +27,7 @@ public class BenchmarkUtility extends CustomUtility {
 	private int delay;
 	private int numberOfGroups;
 	private int numberOfRequestsEachGroup;
+	private int[] delays;
 
 	public BenchmarkUtility(String name) {
 		super(name);
@@ -52,11 +52,11 @@ public class BenchmarkUtility extends CustomUtility {
 	}
 
 	private void logUsage() {
-		logger.fatal("Usage for each mode:\n"
-						+ "(Mode 1) URL JSONINPUT concurrent NUMBER_OF_REQUESTS\n"
-						+ "(Mode 2) URL JSONINPUT sequentialInterval NUMBER_OF_REQUESTS DELAY\n"
-						+ "(Mode 3) URL JSONINPUT sequentailWait NUMBER_OF_REQUESTS DELAY\n"
-						+ "(Mode 4) URL JSONINPUT sequentialConcurrent NUMBER_OF_GROUPS NUMBER_OF_REQUESTS_GROUP DELAY");
+		logger.fatal("Usage for each mode:\n" + "(Mode 1) URL JSONINPUT concurrent NUMBER_OF_REQUESTS\n"
+				+ "(Mode 2) URL JSONINPUT sequentialInterval NUMBER_OF_REQUESTS DELAY\n"
+				+ "(Mode 3) URL JSONINPUT sequentailWait NUMBER_OF_REQUESTS DELAY\n"
+				+ "(Mode 4) URL JSONINPUT sequentialConcurrent NUMBER_OF_GROUPS NUMBER_OF_REQUESTS_GROUP DELAY\n"
+				+ "(Mode 4) URL JSONINPUT sequentialChangingInterval NUMBER_OF_REQUESTS (DELAY)+");
 	}
 
 	private int executeBenchmark() throws SeMoDeException {
@@ -71,22 +71,25 @@ public class BenchmarkUtility extends CustomUtility {
 			return executor.executeSequentialWaitBenchmark(numberOfRequests, delay);
 		case SEQUENTIAL_CONCURRENT:
 			return executor.executeSequentialConcurrentBenchmark(numberOfGroups, numberOfRequestsEachGroup, delay);
+		case SEQUENTIAL_CHANGING_INTERVAL:
+			return executor.executeSequentialChangingIntervalBenchmark(numberOfRequests, delays);
+		case SEQUENTIAL_CHANGING_WAIT:
+			return executor.executeSequentialChangingWaitBenchmark(numberOfRequests, delays);
 		default:
-			throw new SeMoDeException("Mode is not implemented.");
+			throw new SeMoDeException("Mode " + mode + " is not implemented.");
 		}
 	}
 
 	private void initParameters(List<String> args) throws SeMoDeException {
-
+		// minimum amount of arguments
 		validateArgumentSize(args, 4);
 		try {
 			url = new URL(args.get(0));
 			jsonInput = this.readJsonInput(args.get(1));
-			mode = Objects.requireNonNull(BenchmarkMode.fromString(args.get(2)));
+			mode = BenchmarkMode.fromString(args.get(2));
 
 			switch (mode) {
 			case CONCURRENT:
-				validateArgumentSize(args, 4);
 				numberOfRequests = Integer.parseInt(args.get(3));
 				break;
 			case SEQUENTIAL_INTERVAL:
@@ -102,16 +105,27 @@ public class BenchmarkUtility extends CustomUtility {
 				numberOfRequestsEachGroup = Integer.parseInt(args.get(4));
 				delay = Integer.parseInt(args.get(5));
 				break;
+			case SEQUENTIAL_CHANGING_INTERVAL:
+				// Uses the same parameters as SEQUENTIAL_CHANGING_WAIT
+			case SEQUENTIAL_CHANGING_WAIT:
+				validateArgumentSize(args, 5);
+				numberOfRequests = Integer.parseInt(args.get(3));
+				int used = 4;
+				delays = new int[args.size() - used];
+				for (int i = 0; i < args.size() - used; i++) {
+					delays[i] = Integer.parseInt(args.get(used + i));
+				}
+				break;
+			default:
+				throw new SeMoDeException("Mode " + mode + " is not implemented.");
 			}
-		} catch (NullPointerException e) {
-			throw new SeMoDeException("A parameter is null. Check your command.", e);
 		} catch (NumberFormatException e) {
-			throw new SeMoDeException("Number of Requests must be a number.", e);
-		} catch(MalformedURLException e) {
+			throw new SeMoDeException(e.getMessage(), e);
+		} catch (MalformedURLException e) {
 			throw new SeMoDeException("Malformed URL " + args.get(0), e);
 		} catch (IOException | InvalidPathException e) {
-			throw new SeMoDeException("Error by reading the json from the file " + args.get(1), e);
-		} 
+			throw new SeMoDeException("Exception while reading the json from the file " + args.get(1), e);
+		}
 	}
 
 	private void validateArgumentSize(List<String> args, int size) throws SeMoDeException {
