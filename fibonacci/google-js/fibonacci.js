7d91ca2f5ccc04316f706709c1f73ef491db28cb
johannes-manner <johannes.manner@web.de>
2018-08-29 14:49:29 +0200

    deployment of google js works

---
+++ fibonacci/google-js/fibonacci.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const uuidv4 = require('uuid/v4');
+const containerId = uuidv4();
+
+exports.HANDLER = (req, res) => {
+    var n = req.body.n;
+    
+    var requestId = uuidv4();
+
+    if (!n || !isNumeric(n)) {
+        res.status(400).send(createResponse("The parameter is not valid", requestId));
+        return;
+    }
+
+    n = parseInt(n);
+    var result = fib(n);
+
+    res.status(200).send(createResponse(result, requestId));
+};
+
+var fib = function(n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+var createResponse = function (message, requestId) {
+    var response = {
+        result: message,
+        platformId: requestId,
+        containerId: containerId
+    };
+
+    return JSON.stringify(response);
+};
+
+var isNumeric = function(value) {
+    return /^\d+$/.test(value);
+};
