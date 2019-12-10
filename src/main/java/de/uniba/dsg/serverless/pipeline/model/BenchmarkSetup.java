4870d0a9a62130829242143c1ef1d898ce4e4bf6
johannes-manner <johannes.manner@web.de>
2019-12-10 14:04:26 +0100

    changed pipeline and deleted bechmark tests

--- src/main/java/de/uniba/dsg/serverless/pipeline/model/BenchmarkSetup.java
+++ src/main/java/de/uniba/dsg/serverless/pipeline/model/BenchmarkSetup.java
@@ -1,21 +1,16 @@
 package de.uniba.dsg.serverless.pipeline.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Resources;
+import de.uniba.dsg.serverless.model.SeMoDeException;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.io.Resources;
-
-import de.uniba.dsg.serverless.model.SeMoDeException;
 
 /**
  * Model class of a Benchmark Setup. A benchmark setup consists of multiple
@@ -27,112 +22,114 @@ import de.uniba.dsg.serverless.model.SeMoDeException;
  * with keys being listed below.<br>
  * Resulting benchmarking setup contains all possible combinations of the
  * settings.
- * 
  */
 public class BenchmarkSetup {
 
-	public static final String SETUP_LOCATION = "setups";
-	public static final String SEPERATOR = ";";
-	private static final String PIPELINE_JSON = "pipeline.json";
-	private static final String SEMODE_JAR_NAME = "SeMoDe.jar";
+    public static final String SETUP_LOCATION = "setups";
+    public static final String SEPERATOR = ";";
+    private static final String PIPELINE_JSON = "pipeline.json";
+    private static final String SEMODE_JAR_NAME = "SeMoDe.jar";
 
-	public final Config config;
-	public final Map<String, ProviderConfig> possibleProviders;
-	public BenchmarkConfig benchmarkConfig;
-	public final Map<String, ProviderConfig> userProviders;
+    public final Config config;
+    public final Map<String, ProviderConfig> possibleProviders;
+    public final Map<String, ProviderConfig> userProviders;
+    public final String name;
+    public final Path pathToSetup;
+    public final Path pathToConfig;
+    public final Path pathToSources;
+    public final Path pathToDeployment;
+    public final Path pathToEndpoints;
+    public final Path pathToBenchmarkingCommands;
+    public final Path pathToFetchingCommands;
+    public BenchmarkConfig benchmarkConfig;
 
-	public final String name;
-	public final Path pathToSetup;
-	public final Path pathToConfig;
-	public final Path pathToSources;
-	public final Path pathToDeployment;
-	public final Path pathToEndpoints;
-	public final Path pathToBenchmarkingCommands;
-	public final Path pathToFetchingCommands;
+    public BenchmarkSetup(final String name) throws SeMoDeException {
+        this.name = name;
+        this.pathToSetup = Paths.get(BenchmarkSetup.SETUP_LOCATION, name);
+        this.pathToConfig = this.pathToSetup.resolve("settings.json");
+        this.pathToSources = this.pathToSetup.resolve("sources");
+        this.pathToDeployment = this.pathToSetup.resolve("deployments");
+        this.pathToEndpoints = this.pathToSetup.resolve("endpoints");
+        this.pathToBenchmarkingCommands = this.pathToSetup.resolve("benchmarkingCommands");
+        this.pathToFetchingCommands = this.pathToSetup.resolve("fetchingCommands");
+        this.benchmarkConfig = new BenchmarkConfig();
+        this.userProviders = new HashMap<>();
+        this.config = this.loadConfig(PIPELINE_JSON);
+        this.possibleProviders = this.config.getProviderConfigMap();
+    }
 
-	public BenchmarkSetup(String name) throws SeMoDeException {
-		this.name = name;
-		this.pathToSetup = Paths.get(BenchmarkSetup.SETUP_LOCATION, name);
-		this.pathToConfig = pathToSetup.resolve("settings.json");
-		this.pathToSources = pathToSetup.resolve("sources");
-		this.pathToDeployment = pathToSetup.resolve("deployments");
-		this.pathToEndpoints = pathToSetup.resolve("endpoints");
-		this.pathToBenchmarkingCommands = pathToSetup.resolve("benchmarkingCommands");
-		this.pathToFetchingCommands = pathToSetup.resolve("fetchingCommands");
-		this.benchmarkConfig = new BenchmarkConfig();
-		this.userProviders = new HashMap<>();
-		this.config = loadConfig(PIPELINE_JSON);
-		this.possibleProviders = config.getProviderConfigMap();
-	}
+    public boolean setupAlreadyExists() {
+        return Files.exists(this.pathToSetup);
+    }
 
-	private Config loadConfig(String path) throws SeMoDeException {
-		ObjectMapper om = new ObjectMapper();
-		try {
-			return om.readValue(Resources.toString(Resources.getResource(path), StandardCharsets.UTF_8), Config.class);
-		} catch (IOException e) {
-			throw new SeMoDeException("Error while parsing the " + path + " file. Check the config.");
-		}
-	}
+    private Config loadConfig(final String path) throws SeMoDeException {
+        final ObjectMapper om = new ObjectMapper();
+        try {
+            return om.readValue(Resources.toString(Resources.getResource(path), StandardCharsets.UTF_8), Config.class);
+        } catch (final IOException e) {
+            throw new SeMoDeException("Error while parsing the " + path + " file. Check the config.");
+        }
+    }
 
-	/**
-	 * Load the user config files.
-	 * 
-	 * @return
-	 * @throws SeMoDeException
-	 */
-	public void loadUserConfig(String path) throws SeMoDeException {
-		ObjectMapper om = new ObjectMapper();
-		try {
-			UserConfig config = om.readValue(Paths.get(path).toFile(), UserConfig.class);
-			Map<String, ProviderConfig> map = new HashMap<>();
-			for (ProviderConfig provider : config.getProviderConfigs()) {
-				map.put(provider.getName(), provider);
-			}
+    /**
+     * Load the user config files.
+     *
+     * @return
+     * @throws SeMoDeException
+     */
+    public void loadUserConfig(final String path) throws SeMoDeException {
+        final ObjectMapper om = new ObjectMapper();
+        try {
+            final UserConfig config = om.readValue(Paths.get(path).toFile(), UserConfig.class);
+            final Map<String, ProviderConfig> map = new HashMap<>();
+            for (final ProviderConfig provider : config.getProviderConfigs()) {
+                map.put(provider.getName(), provider);
+            }
 
-			// initialize all user config variables
-			this.userProviders.putAll(map);
-			this.benchmarkConfig = config.getBenchmarkConfig();
+            // initialize all user config variables
+            this.userProviders.putAll(map);
+            this.benchmarkConfig = config.getBenchmarkConfig();
 
-		} catch (IOException e) {
-			throw new SeMoDeException("Error while parsing the " + path + " file. Check the config.");
-		}
-	}
+        } catch (final IOException e) {
+            throw new SeMoDeException("Error while parsing the " + path + " file. Check the config.");
+        }
+    }
 
-	public UserConfig assembleUserConfig() {
-		List<ProviderConfig> providerConfigs = new ArrayList<>();
-		for (ProviderConfig config : this.userProviders.values()) {
-			providerConfigs.add(config);
-		}
-		return new UserConfig(providerConfigs, this.benchmarkConfig);
-	}
+    public UserConfig assembleUserConfig() {
+        final List<ProviderConfig> providerConfigs = new ArrayList<>();
+        for (final ProviderConfig config : this.userProviders.values()) {
+            providerConfigs.add(config);
+        }
+        return new UserConfig(providerConfigs, this.benchmarkConfig);
+    }
 
-	/**
-	 * This functions return the fully qualified name of the SeMoDe.jar file to
-	 * enable other utilities to generate batch files for automating the
-	 * benchmarking pipeline.
-	 * 
-	 * @return the location of the SeMoDe.jar file to generate different batch files
-	 * @throws SeMoDeException if the SeMoDe.jar is not in the current project
-	 *                         directory.
-	 */
-	public String getSeMoDeJarLocation() throws SeMoDeException {
+    /**
+     * This functions return the fully qualified name of the SeMoDe.jar file to
+     * enable other utilities to generate batch files for automating the
+     * benchmarking pipeline.
+     *
+     * @return the location of the SeMoDe.jar file to generate different batch files
+     * @throws SeMoDeException if the SeMoDe.jar is not in the current project
+     *                         directory.
+     */
+    public String getSeMoDeJarLocation() throws SeMoDeException {
 
-		Predicate<Path> isSeMoDeJar = p -> p.toString().endsWith(SEMODE_JAR_NAME);
-		Optional<String> jarFile;
-		try {
-			jarFile = Files.walk(this.pathToSetup.toAbsolutePath().getParent().getParent())
-					.filter(isSeMoDeJar)
-					.map(p -> p.toString())
-					.findFirst();
+        final Predicate<Path> isSeMoDeJar = p -> p.toString().endsWith(SEMODE_JAR_NAME);
+        final Optional<String> jarFile;
+        try {
+            jarFile = Files.walk(this.pathToSetup.toAbsolutePath().getParent().getParent())
+                    .filter(isSeMoDeJar)
+                    .map(p -> p.toString())
+                    .findFirst();
 
-			// there is only one SeMoDe-jar
-			if (!jarFile.isPresent()) {
-				throw new SeMoDeException(
-						"The SeMoDe utility was not built - please execute the gradle build command before executing the command again");
-			}
-			return jarFile.get();
-		} catch (IOException e) {
-			throw new SeMoDeException("Error while traversing the SeMoDe file tree", e);
-		}
-	}
+            // there is only one SeMoDe-jar
+            if (!jarFile.isPresent()) {
+                throw new SeMoDeException(
+                        "The SeMoDe utility was not built - please execute the gradle build command before executing the command again");
+            }
+            return jarFile.get();
+        } catch (final IOException e) {
+            throw new SeMoDeException("Error while traversing the SeMoDe file tree", e);
+        }
+    }
 }
--- src/main/java/de/uniba/dsg/serverless/pipeline/utils/BenchmarkingCommandGenerator.java
+++ src/main/java/de/uniba/dsg/serverless/pipeline/utils/BenchmarkingCommandGenerator.java
@@ -1,5 +1,8 @@
 package de.uniba.dsg.serverless.pipeline.utils;
 
+import de.uniba.dsg.serverless.model.SeMoDeException;
+import de.uniba.dsg.serverless.pipeline.model.BenchmarkConfig;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -8,50 +11,49 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-import de.uniba.dsg.serverless.model.SeMoDeException;
-import de.uniba.dsg.serverless.pipeline.model.BenchmarkConfig;
-
 public class BenchmarkingCommandGenerator {
 
-	private final Path benchmarkCommandPath;
-	private final Path endpointsPath;
-	private final BenchmarkConfig benchmarkConfig;
-	private final String seMoDeJarLocation;
+    private final Path benchmarkCommandPath;
+    private final Path endpointsPath;
+    private final BenchmarkConfig benchmarkConfig;
+    private final String seMoDeJarLocation;
 
-	public BenchmarkingCommandGenerator(Path benchmarkCommandPath, Path endpointsPath, BenchmarkConfig benchmarkConfig, String seMoDeJarLocation) {
-		this.benchmarkCommandPath = benchmarkCommandPath;
-		this.endpointsPath = endpointsPath;
-		this.benchmarkConfig = benchmarkConfig;
-		this.seMoDeJarLocation = seMoDeJarLocation;
-	}
+    public BenchmarkingCommandGenerator(final Path benchmarkCommandPath, final Path endpointsPath, final BenchmarkConfig benchmarkConfig, final String seMoDeJarLocation) {
+        this.benchmarkCommandPath = benchmarkCommandPath;
+        this.endpointsPath = endpointsPath;
+        this.benchmarkConfig = benchmarkConfig;
+        this.seMoDeJarLocation = seMoDeJarLocation;
+    }
 
-	public void generateCommands(String language, String provider) throws SeMoDeException {
-		
-		String providerLanguage = provider + "-" + language;
+    public void generateCommands(final String language, final String provider) throws SeMoDeException {
 
-		Path outputCommands = Paths.get(this.benchmarkCommandPath.toString(), providerLanguage + ".bat");
-		Path inputEndpoints = Paths.get(this.endpointsPath.toString(), providerLanguage);
+        final String providerLanguage = provider + "-" + language;
 
-		String firstPart = "start cmd /k java -jar " + this.seMoDeJarLocation + " benchmark";
-		// TODO params.json - how to deal with this file - locate in benchmarking commands folder
-		String jsonInput = "params.json";
+        final Path outputCommands = Paths.get(this.benchmarkCommandPath.toString(), providerLanguage + ".bat");
+        final Path inputEndpoints = Paths.get(this.endpointsPath.toString(), providerLanguage);
 
-		try (BufferedWriter writer = Files.newBufferedWriter(outputCommands, StandardOpenOption.CREATE,
-				StandardOpenOption.TRUNCATE_EXISTING)) {
-			try (BufferedReader reader = Files.newBufferedReader(inputEndpoints)) {
-				String line = reader.readLine();
-				while (line != null) {
-					String[] functionPlusUrl = line.split(" ");
-					String command = firstPart + " " + provider + "_" + functionPlusUrl[0] + " " + functionPlusUrl[1] + " " + jsonInput
-							+ " "  + this.benchmarkConfig.getBenchmarkMode() + " "  + this.benchmarkConfig.getNumberThreads() + " " + this.benchmarkConfig.getBenchmarkParameters();
-					writer.write(command);
-					writer.newLine();
-					line = reader.readLine();
-				}
-			}
-		} catch (IOException e) {
-			throw new SeMoDeException("Something went wrong during file operations. " + e.getMessage(), e); 
-		}
-	}
+        // TODO test xterm under Windows (Git Bash)
+        final String firstPart = "xterm -e 'sh -c \"java -jar " + this.seMoDeJarLocation + " benchmark";
+        // TODO params.json - how to deal with this file - locate in benchmarking commands folder
+        final String jsonInput = "params.json";
+
+        try (final BufferedWriter writer = Files.newBufferedWriter(outputCommands, StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING)) {
+            try (final BufferedReader reader = Files.newBufferedReader(inputEndpoints)) {
+                String line = reader.readLine();
+                while (line != null) {
+                    final String[] functionPlusUrl = line.split(" ");
+                    final String command = firstPart + " " + provider + "_" + functionPlusUrl[0] + " " + functionPlusUrl[1] + " " + jsonInput
+                            + " " + this.benchmarkConfig.getBenchmarkMode() + " " + this.benchmarkConfig.getNumberThreads() + " " + this.benchmarkConfig.getBenchmarkParameters()
+                            + "\"'";
+                    writer.write(command);
+                    writer.newLine();
+                    line = reader.readLine();
+                }
+            }
+        } catch (final IOException e) {
+            throw new SeMoDeException("Something went wrong during file operations. " + e.getMessage(), e);
+        }
+    }
 
 }
