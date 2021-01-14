23f10d9b3d31e242d489a977e60551fc37473b7a
Johannes Manner <johannes.manner@web.de>
2021-01-14 16:16:36 +0100

    to the prior commit :)

--- src/main/java/de/uniba/dsg/serverless/ArgumentProcessor.java
+++ src/main/java/de/uniba/dsg/serverless/ArgumentProcessor.java
@@ -1,19 +1,34 @@
 package de.uniba.dsg.serverless;
 
-import de.uniba.dsg.serverless.cli.CustomUtility;
-import de.uniba.dsg.serverless.cli.UtilityFactory;
-import de.uniba.dsg.serverless.util.FileLogger;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ArgumentProcessor {
+import de.uniba.dsg.serverless.cli.CustomUtility;
+import de.uniba.dsg.serverless.cli.UtilityFactory;
+import de.uniba.dsg.serverless.util.FileLogger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Run the application is a command line utility by specifying a that the app is not a web application. <br/>
+ * <b>java -jar -Dspring.main.web-application-type=NONE app.jar</b>
+ * </p>
+ *
+ * Source: <a href="https://stackoverflow.com/questions/45615729/how-to-run-spring-boot-application-both-as-a-web-application-as-well-as-a-comman/45616851">Stack
+ * Overflow Post</a>
+ */
+@Component
+@ConditionalOnNotWebApplication
+public class ArgumentProcessor implements CommandLineRunner {
 
     // a global logger for the whole program
     public static FileLogger logger;
 
-    public static void main(final String[] args) {
+    @Override
+    public void run(final String[] args) {
         // set logging system property
         // TODO think about property management
         System.setProperty("java.util.logging.SimpleFormatter.format", "[;%1$tFT%1$tH:%1$tM:%1$tS.%1$tL;] [%4$-7s] %5$s %n");
--- src/main/java/de/uniba/dsg/serverless/SeMoDeApplication.java
+++ src/main/java/de/uniba/dsg/serverless/SeMoDeApplication.java
@@ -1,4 +1,11 @@
 package de.uniba.dsg.serverless;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SeMoDeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeMoDeApplication.class, args);
+    }
 }
