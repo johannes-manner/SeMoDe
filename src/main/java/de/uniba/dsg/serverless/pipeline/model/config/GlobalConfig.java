7d46405347b458c27565465298d72ca0d19f9bb6
Johannes Manner <johannes.manner@web.de>
2021-01-15 09:59:01 +0100

    remove language config

--- src/main/java/de/uniba/dsg/serverless/pipeline/model/config/GlobalConfig.java
+++ src/main/java/de/uniba/dsg/serverless/pipeline/model/config/GlobalConfig.java
@@ -1,31 +1,10 @@
 package de.uniba.dsg.serverless.pipeline.model.config;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class GlobalConfig {
 
-    private List<LanguageConfig> languageConfigs;
     private CalibrationConfig calibrationConfig;
 
     public GlobalConfig() {
-    }
-
-    public Map<String, LanguageConfig> getLanguageConfigMap() {
-        final Map<String, LanguageConfig> languageMap = new HashMap<>();
-        for (final LanguageConfig language : this.languageConfigs) {
-            languageMap.put(language.getIdentifier(), language);
-        }
-        return languageMap;
-    }
-
-    public List<LanguageConfig> getLanguageConfigs() {
-        return this.languageConfigs;
-    }
-
-    public void setLanguageConfigs(final List<LanguageConfig> languageConfigs) {
-        this.languageConfigs = languageConfigs;
     }
 
     public CalibrationConfig getCalibrationConfig() {
@@ -36,11 +15,9 @@ public class GlobalConfig {
         this.calibrationConfig = calibrationConfig;
     }
 
-
     @Override
     public String toString() {
         return "Config{" +
-                ", languageConfigs=" + this.languageConfigs +
                 ", calibrationConfig=" + this.calibrationConfig +
                 '}';
     }
