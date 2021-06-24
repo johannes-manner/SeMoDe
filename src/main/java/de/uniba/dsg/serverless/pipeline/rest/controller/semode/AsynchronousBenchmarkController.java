822461847dc5a05dd821064bf993fb457eaed8fe
Johannes Manner <johannes.manner@web.de>
2021-06-24 17:22:53 +0200

    improve security for semode api

--- src/main/java/de/uniba/dsg/serverless/pipeline/rest/controller/semode/AsynchronousBenchmarkController.java
+++ src/main/java/de/uniba/dsg/serverless/pipeline/rest/controller/semode/AsynchronousBenchmarkController.java
@@ -5,9 +5,12 @@ import de.uniba.dsg.serverless.pipeline.model.config.BenchmarkConfig;
 import de.uniba.dsg.serverless.pipeline.repo.projection.IPointDto;
 import de.uniba.dsg.serverless.pipeline.service.SetupService;
 import de.uniba.dsg.serverless.pipeline.util.SeMoDeException;
+import de.uniba.dsg.serverless.users.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,28 +24,53 @@ public class AsynchronousBenchmarkController {
     private SetupService service;
 
     @GetMapping("semode/v1/{setup}/benchmark/deploy")
-    public ResponseEntity deployFunction(@PathVariable("setup") String setup) throws SeMoDeException {
+    public ResponseEntity deployFunction(@PathVariable("setup") String setupName,
+                                         @AuthenticationPrincipal User user) throws SeMoDeException {
         log.info("Start deployment...");
-        this.service.deployFunctions(setup);
-        return ResponseEntity.ok().build();
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.deployFunctions(setupName);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/benchmark/undeploy")
-    public ResponseEntity undeployFunction(@PathVariable("setup") String setup) throws SeMoDeException {
-        this.service.undeployFunctions(setup);
-        return ResponseEntity.ok().build();
+    public ResponseEntity undeployFunction(@PathVariable("setup") String setupName,
+                                           @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.undeployFunctions(setupName);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
     }
 
     @GetMapping("semode/v1/{setup}/benchmark/execute")
-    public ResponseEntity benchmark(@PathVariable("setup") String setup) throws SeMoDeException {
-        this.service.executeBenchmark(setup);
-        return ResponseEntity.ok().build();
+    public ResponseEntity benchmark(@PathVariable("setup") String setupName,
+                                    @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.executeBenchmark(setupName);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/benchmark/fetch")
-    public ResponseEntity fetch(@PathVariable("setup") String setup) throws SeMoDeException {
-        this.service.fetchPerformanceData(setup);
-        return ResponseEntity.ok().build();
+    public ResponseEntity fetch(@PathVariable("setup") String setupName,
+                                @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.fetchPerformanceData(setupName);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/benchmark/mode/{tag}")
@@ -51,24 +79,54 @@ public class AsynchronousBenchmarkController {
     }
 
     @GetMapping("semode/v1/{setup}/benchmark/version/{version}")
-    public BenchmarkConfig getBenchmarkConfigByVersion(@PathVariable("setup") String setup, @PathVariable(value = "version") Integer version) {
-        return this.service.getBenchmarkConfigBySetupAndVersion(setup, version);
+    public ResponseEntity<BenchmarkConfig> getBenchmarkConfigByVersion(@PathVariable("setup") String setupName,
+                                                                       @PathVariable(value = "version") Integer version,
+                                                                       @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            return ResponseEntity.ok(this.service.getBenchmarkConfigBySetupAndVersion(setupName, version));
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
     }
 
     @GetMapping("semode/v1/{setup}/benchmark/version/{version}/data")
-    public IPointDto[] getBenchmarkData(@PathVariable(value = "setup") String setupName, @PathVariable(value = "version") Integer version) {
-        return this.service.getBenchmarkDataByVersion(setupName, version);
+    public ResponseEntity<IPointDto[]> getBenchmarkData(@PathVariable(value = "setup") String setupName,
+                                                        @PathVariable(value = "version") Integer version,
+                                                        @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            return ResponseEntity.ok(this.service.getBenchmarkDataByVersion(setupName, version));
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @PostMapping("semode/v1/{setup}/benchmark/visible/{version}")
-    public ResponseEntity changePublicVisibility(@PathVariable(value = "setup") String setupName, @PathVariable(value = "version") int version) {
-        this.service.changePublicVisiblityPropertyForBenchmarkVersion(setupName, version);
-        return ResponseEntity.ok().build();
+    public ResponseEntity changePublicVisibility(@PathVariable(value = "setup") String setupName,
+                                                 @PathVariable(value = "version") int version,
+                                                 @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.changePublicVisiblityPropertyForBenchmarkVersion(setupName, version);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @PostMapping("semode/v1/{setup}/benchmark/description/{version}")
-    public ResponseEntity changeBenchmarkDescription(@PathVariable(value = "setup") String setupName, @PathVariable(value = "version") int version, String newDescription) {
-        this.service.changeDescriptionForBenchmarkVersion(setupName, version, newDescription);
-        return ResponseEntity.ok().build();
+    public ResponseEntity changeBenchmarkDescription(@PathVariable(value = "setup") String setupName,
+                                                     @PathVariable(value = "version") int version,
+                                                     String newDescription,
+                                                     @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.changeDescriptionForBenchmarkVersion(setupName, version, newDescription);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 }
--- src/main/java/de/uniba/dsg/serverless/pipeline/rest/controller/semode/AsynchronousCalibrationController.java
+++ src/main/java/de/uniba/dsg/serverless/pipeline/rest/controller/semode/AsynchronousCalibrationController.java
@@ -5,9 +5,12 @@ import de.uniba.dsg.serverless.pipeline.repo.projection.ICalibrationConfigId;
 import de.uniba.dsg.serverless.pipeline.repo.projection.IPointDto;
 import de.uniba.dsg.serverless.pipeline.service.SetupService;
 import de.uniba.dsg.serverless.pipeline.util.SeMoDeException;
+import de.uniba.dsg.serverless.users.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,54 +25,115 @@ public class AsynchronousCalibrationController {
     private SetupService service;
 
     @GetMapping("semode/v1/{setup}/calibration/mapping")
-    public ResponseEntity<String> mapping(@PathVariable(value = "setup") String setupName) throws SeMoDeException {
-        String mappingResult = this.service.computeMapping(setupName).toString();
-        log.info("Mapping: " + mappingResult);
-        return ResponseEntity.ok(mappingResult);
+    public ResponseEntity<String> mapping(@PathVariable(value = "setup") String setupName,
+                                          @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            String mappingResult = this.service.computeMapping(setupName).toString();
+            log.info("Mapping: " + mappingResult);
+            return ResponseEntity.ok(mappingResult);
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/calibration/version/{version}")
-    public CalibrationConfig getCalibrationConfig(@PathVariable(value = "setup") String setup, @PathVariable(value = "version") Integer version) {
-        return this.service.getCalibrationBySetupAndVersion(setup, version);
+    public ResponseEntity<CalibrationConfig> getCalibrationConfig(@PathVariable(value = "setup") String setupName,
+                                                                  @PathVariable(value = "version") Integer version,
+                                                                  @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            return ResponseEntity.ok(this.service.getCalibrationBySetupAndVersion(setupName, version));
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/calibration/{calibrationId}/data")
-    public IPointDto[] getCalibrationData(@PathVariable(value = "setup") String setup, @PathVariable(value = "calibrationId") Integer calibrationId) {
-        return this.service.getCalibrationDataBySetupAndId(setup, calibrationId);
+    public ResponseEntity<IPointDto[]> getCalibrationData(@PathVariable(value = "setup") String setupName,
+                                                          @PathVariable(value = "calibrationId") Integer calibrationId,
+                                                          @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            return ResponseEntity.ok(this.service.getCalibrationDataBySetupAndId(setupName, calibrationId));
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/profiles")
-    public List<ICalibrationConfigId> getProfilesForSetup(@PathVariable(value = "setup") String setup) {
-        return this.service.getProfilesForSetup(setup);
+    public ResponseEntity<List<ICalibrationConfigId>> getProfilesForSetup(@PathVariable(value = "setup") String setupName,
+                                                                          @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            return ResponseEntity.ok(this.service.getProfilesForSetup(setupName));
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/profiles/{calibrationConfigId}")
-    public IPointDto[] getProfilePointsForSetupAndCalibration(@PathVariable(value = "setup") String setup, @PathVariable("calibrationConfigId") Integer id) {
-        return this.service.getProfilePointsForSetupAndCalibration(setup, id).toArray(IPointDto[]::new);
+    public ResponseEntity<IPointDto[]> getProfilePointsForSetupAndCalibration(@PathVariable(value = "setup") String setupName,
+                                                                              @PathVariable("calibrationConfigId") Integer id,
+                                                                              @AuthenticationPrincipal User user) {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            return ResponseEntity.ok(this.service.getProfilePointsForSetupAndCalibration(setupName, id).toArray(IPointDto[]::new));
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/calibration/start/{platform}")
-    public ResponseEntity startCalibration(@PathVariable(value = "setup") String setup, @PathVariable("platform") String platform) throws SeMoDeException {
-        this.service.startCalibration(setup, platform);
-        return ResponseEntity.ok().build();
+    public ResponseEntity startCalibration(@PathVariable(value = "setup") String setupName,
+                                           @PathVariable("platform") String platform,
+                                           @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.startCalibration(setupName, platform);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
     }
 
     @GetMapping("semode/v1/{setup}/calibration/deploy/{platform}")
-    public ResponseEntity deployCalibration(@PathVariable(value = "setup") String setup, @PathVariable("platform") String platform) throws SeMoDeException {
-        this.service.deployCalibration(setup, platform);
-        return ResponseEntity.ok().build();
+    public ResponseEntity deployCalibration(@PathVariable(value = "setup") String setupName,
+                                            @PathVariable("platform") String platform,
+                                            @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.deployCalibration(setupName, platform);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("semode/v1/{setup}/calibration/undeploy/{platform}")
-    public ResponseEntity undeployCalibration(@PathVariable(value = "setup") String setup, @PathVariable("platform") String platform) throws SeMoDeException {
-        this.service.undeployCalibration(setup, platform);
-        return ResponseEntity.ok().build();
+    public ResponseEntity undeployCalibration(@PathVariable(value = "setup") String setupName,
+                                              @PathVariable("platform") String platform,
+                                              @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.undeployCalibration(setupName, platform);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
 
     @GetMapping("semode/v1/{setup}/simulation/run")
-    public ResponseEntity runFunction(@PathVariable(value = "setup") String setup) throws SeMoDeException {
-        this.service.runFunctionLocally(setup);
-        return ResponseEntity.ok().build();
+    public ResponseEntity runFunction(@PathVariable(value = "setup") String setupName,
+                                      @AuthenticationPrincipal User user) throws SeMoDeException {
+        if (this.service.checkSetupAccessRights(setupName, user)) {
+            this.service.runFunctionLocally(setupName);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Access from user '" + user.getUsername() + "' for setup: " + setupName);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 }
